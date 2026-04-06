cf2272d584d860c7ce4d517f57796ca093b4f071
Léopold Gravier <leopold.gravier@etu.minesparis.psl.eu>
2026-04-06 20:19:38 +0200

    Setup react-dsfr

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+startReactDsfr({ defaultColorScheme: "system" });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
